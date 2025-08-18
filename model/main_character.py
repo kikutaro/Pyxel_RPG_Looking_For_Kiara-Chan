ad5144c9b9ae7133f4f6201ed0fae5f55ec0e995
kikutaro <kikutaro526@yahoo.co.jp>
2025-08-18 17:32:57 +0900

    issue65に対応

--- model/main_character.py
+++ model/main_character.py
@@ -60,7 +60,7 @@ class MainCharacter(Character):
 
         if pyxel.btnp(pyxel.GAMEPAD1_BUTTON_DPAD_LEFT,const.HOLD,const.REPEAT) or pyxel.btnp(pyxel.KEY_LEFT,const.HOLD,const.REPEAT):
                 self.向き = const.向き.西
-                if self.前に進めるかな(self.global_x+4 - const.キャラ歩幅, self.global_y, self.global_x+4 - const.キャラ歩幅, self.global_y + const.キャラサイズ-1) and not self.メンバーとの接触チェック(self.local_x - const.キャラ歩幅, self.local_y):
+                if self.前に進めるかな(self.global_x+4 - const.当たり判定マージン, self.global_y, self.global_x+4 - const.当たり判定マージン, self.global_y + const.キャラサイズ-1) and not self.メンバーとの接触チェック(self.local_x - const.当たり判定マージン, self.local_y):
                     self.global_x -= const.キャラ歩幅
 
                     self.足元チェック(self.global_x, self.global_y)
@@ -71,8 +71,7 @@ class MainCharacter(Character):
                         self.global_x -= const.キャラサイズ
         elif pyxel.btnp(pyxel.GAMEPAD1_BUTTON_DPAD_RIGHT,const.HOLD,const.REPEAT) or pyxel.btnp(pyxel.KEY_RIGHT,const.HOLD,const.REPEAT):
                 self.向き = const.向き.東
-                if self.前に進めるかな(self.global_x + const.キャラサイズ-6 + const.キャラ歩幅, self.global_y, self.global_x + const.キャラサイズ-6 + const.キャラ歩幅, self.global_y + const.キャラサイズ-1) and not self.メンバーとの接触チェック(self.local_x + const.キャラ歩幅, self.local_y):
-                    # self.local_x += const.キャラ歩幅
+                if self.前に進めるかな(self.global_x + const.キャラサイズ-6 + const.当たり判定マージン, self.global_y, self.global_x + const.キャラサイズ-6 + const.当たり判定マージン, self.global_y + const.キャラサイズ-1) and not self.メンバーとの接触チェック(self.local_x + const.当たり判定マージン, self.local_y):
                     self.global_x += const.キャラ歩幅
 
                     self.足元チェック(self.global_x, self.global_y)
@@ -83,8 +82,7 @@ class MainCharacter(Character):
                         self.global_x += const.キャラサイズ
         elif pyxel.btnp(pyxel.GAMEPAD1_BUTTON_DPAD_UP,const.HOLD,const.REPEAT) or pyxel.btnp(pyxel.KEY_UP,const.HOLD,const.REPEAT):
                 self.向き = const.向き.北
-                if self.前に進めるかな(self.global_x+5, self.global_y - const.キャラ歩幅, self.global_x + const.キャラサイズ-5, self.global_y - const.キャラ歩幅) and not self.メンバーとの接触チェック(self.local_x, self.local_y - const.キャラ歩幅):
-                    # self.local_y -= const.キャラ歩幅
+                if self.前に進めるかな(self.global_x+5, self.global_y - const.当たり判定マージン, self.global_x + const.キャラサイズ-5, self.global_y - const.当たり判定マージン) and not self.メンバーとの接触チェック(self.local_x, self.local_y - const.当たり判定マージン):
                     self.global_y -= const.キャラ歩幅
 
                     self.足元チェック(self.global_x, self.global_y)
@@ -95,8 +93,7 @@ class MainCharacter(Character):
                         self.global_y -= const.キャラサイズ
         elif pyxel.btnp(pyxel.GAMEPAD1_BUTTON_DPAD_DOWN,const.HOLD,const.REPEAT) or pyxel.btnp(pyxel.KEY_DOWN,const.HOLD,const.REPEAT):
                 self.向き = const.向き.南
-                if self.前に進めるかな(self.global_x+5, self.global_y + const.キャラサイズ + const.キャラ歩幅, self.global_x + const.キャラサイズ-5 , self.global_y + const.キャラサイズ + const.キャラ歩幅) and not self.メンバーとの接触チェック(self.local_x, self.local_y + const.キャラ歩幅 ):
-                    # self.local_y += const.キャラ歩幅
+                if self.前に進めるかな(self.global_x+5, self.global_y + const.キャラサイズ + const.当たり判定マージン, self.global_x + const.キャラサイズ-5 , self.global_y + const.キャラサイズ + const.当たり判定マージン) and not self.メンバーとの接触チェック(self.local_x, self.local_y + const.当たり判定マージン ):
                     self.global_y += const.キャラ歩幅
 
                     self.足元チェック(self.global_x, self.global_y)
